53a0c3737dec49aeb130251012b63a8fdd6414fe
Puckley77 <prestonbuckley@gmail.com>
2019-05-28 10:29:49 -0600

    final commit for automated tests

--- tests/searchTests.js
+++ tests/searchTests.js
@@ -1,6 +1,6 @@
 var pageObjects = {}
-let navCat= require('../pageAssets/navCatAssets')
-let rent= require('../pageAssets/rentAssests')
+let navCat = require('../pageAssets/navCatAssets')
+let rent = require('../pageAssets/rentAssests')
 module.exports = {
     beforeEach: browser => {
         pageObjects = browser.page.yoodlizePO()
@@ -15,36 +15,38 @@ module.exports = {
         .navCat(navCat)
     },
 
-    'Rent Item and Deny Request': browser => {
-        rent.forEach(test => {
-        pageObjects
-        .logIn('testing.yoodlize1@gmail.com')
-        .search(test.item)
-        .request('11/22/2019','3')
-        .deny()
-        .logout()
-    })
-},
+        'Rent Item and Deny Request': browser => {
+            rent.forEach(test => {
+            pageObjects
+            .logIn('testing.yoodlize1@gmail.com')
+            .search(test.item)
+            .request('11/22/2019','3')
+            .deny()
+            .logout()
+        })
+    },
     'Search is available on all pages': browser => {
+        pageObjects
+            .api.resizeWindow(960, 1080)
         pageObjects
             .waitForElementVisible('@navSearch')
             .pause(100)
             .verify.containsText('@navSearch', 'Search')
         pageObjects
-        .pause(100)
+            .pause(100)
             .click('@navWorks')
             .pause(100)
             .waitForElementVisible('@navSearch')
             .pause(100)
             .verify.containsText('@navSearch', 'Search')
         pageObjects
-        .pause(100)
+            .pause(100)
             .click('@navSearch')
             .pause(100)
             .waitForElementVisible('@navSearch')
             .pause(100)
         pageObjects
-        .pause(100)
+            .pause(100)
             .useXpath()
             .click('//*[text()="List an Item"]')
             .useCss()
@@ -53,7 +55,7 @@ module.exports = {
             .pause(100)
             .verify.containsText('@navSearch', 'Search')
         pageObjects
-        .pause(100)
+            .pause(100)
             .useXpath()
             .click('//*[text()="Terms"]')
             .useCss()
@@ -62,7 +64,7 @@ module.exports = {
             .pause(100)
             .verify.containsText('@navSearch', 'Search')
         pageObjects
-        .pause(100)
+            .pause(100)
             .useXpath()
             .click('//*[text()="Privacy"]')
             .useCss()
@@ -71,43 +73,43 @@ module.exports = {
             .pause(100)
             .verify.containsText('@navSearch', 'Search')
         pageObjects
-        .pause(100)
+            .pause(100)
             .logIn('testing.yoodlize1@gmail.com')
-            .useXpath() 
+            .useXpath()
             .click('//*[text()="List Your Stuff"]')
             .useCss()
             .pause(100)
             .waitForElementVisible('@sSearch')
             .pause(100)
-            pageObjects
+        pageObjects
             .useXpath()
             .click('//i[@class="fal fa-comments hidden-md hidden-lg hidden-xs"]')
             .useCss()
             .pause(100)
             .waitForElementVisible('@sSearch')
             .pause(100)
-            pageObjects
+        pageObjects
             .useXpath()
             .click('(//*[text()="My Listings"])[2]')
             .useCss()
             .pause(100)
             .waitForElementVisible('@sSearch')
             .pause(100)
-            pageObjects
+        pageObjects
             .useXpath()
             .click('(//*[text()="Reviews"])[2]')
             .useCss()
             .pause(100)
             .waitForElementVisible('@sSearch')
             .pause(100)
-            pageObjects
+        pageObjects
             .useXpath()
             .click('(//*[text()="Profile"])[1]')
             .useCss()
             .pause(100)
             .waitForElementVisible('@sSearch')
             .pause(100)
-            pageObjects
+        pageObjects
             .useXpath()
             .click('//*[text()="Account"]')
             .useCss()
